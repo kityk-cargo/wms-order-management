aa9e01451b9ef012b9303abdbd4273550fe2812a
dsmd <lex.mironchenko@gmail.com>
2025-02-27 04:51:08 +0000

    fixed startup/test exec

--- src/main/java/cargo/kityk/wms/order/OpenApiGeneratorApplication.java
+++ src/main/java/cargo/kityk/wms/order/OpenApiGeneratorApplication.java
@@ -1,17 +1,16 @@
 package cargo.kityk.wms.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-        // Don't exclude the main application class
-    }
-)
+@Configuration
+@EnableAutoConfiguration(exclude = {
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 @ComponentScan(basePackages = "cargo.kityk.wms") // Ensure we scan all components
 public class OpenApiGeneratorApplication {
     public static void main(String[] args) {
