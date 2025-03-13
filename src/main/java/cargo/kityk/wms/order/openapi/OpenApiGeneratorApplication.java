3633709e71da31d2cc83fcc8945f2ee57132ff59
dsmd <lex.mironchenko@gmail.com>
2025-03-13 01:21:18 +0000

    fixed openapi generation on release

--- src/main/java/cargo/kityk/wms/order/openapi/OpenApiGeneratorApplication.java
+++ src/main/java/cargo/kityk/wms/order/openapi/OpenApiGeneratorApplication.java
@@ -1,12 +1,17 @@
 package cargo.kityk.wms.order.openapi;
 
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.MergedAnnotation;
+
+import static org.springframework.context.annotation.AnnotationConfigUtils.AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME;
 
 @SpringBootApplication
 @Profile("openapi")
@@ -19,6 +24,29 @@ public class OpenApiGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(OpenApiGeneratorApplication.class);
         app.setWebApplicationType(WebApplicationType.SERVLET);
+        app.setAllowBeanDefinitionOverriding(true);
         app.run(args);
+    }
+
+    /**
+     * The name is needed here because this bean is already loaded with such name at ComponentScan processing time.
+     * We reload this bean with our custom processor to turn off mandatory autowiring.
+     * @return
+     */
+    @Bean(name = AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME)
+    @Primary
+    public static AutowiredAnnotationBeanPostProcessor autowiredAnnotationBeanPostProcessor() {
+        return new CustomAutowiredAnnotationBeanPostProcessor();
+    }
+
+    /**
+     * Custom AutowiredAnnotationBeanPostProcessor that turns off autowiring by forcing
+     * all @Autowired annotations to act as if required=false.
+     */
+    private static class CustomAutowiredAnnotationBeanPostProcessor extends AutowiredAnnotationBeanPostProcessor {
+        @Override
+        protected boolean determineRequiredStatus(MergedAnnotation<?> ann) {
+            return false;
+        }
     }
 }
